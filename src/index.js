aee27657673a03b9523006b566388e74523db754
Nikita Polyakov <nikita.polyakov.1997@yandex.ru>
2024-08-14 12:06:49 +0300

    modified:   src/blocks/popup/__button/popup__button.css 	modified:   src/blocks/popup/__input/popup__input.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const inputElement = formElement.querySelector('.popup__input');
 const jobInput = document.querySelector('.popup__input_type_description');
 const title = document.querySelector('.profile__title');
 const job = document.querySelector('.profile__description');
+const avatar = document.querySelector('.profile__image');
 const formEditProfile = document.forms['edit-profile'];
 const formNewCard = document.forms['new-place'];
 const newCardNameInput = document.querySelector('.popup__input_type_card-name');
@@ -25,9 +26,9 @@ const openImageCardPopup = document.querySelector('.popup__image');
 const imageModal = document.querySelector('.popup_type_image');
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(element) {
+/*initialCards.forEach(function(element) {
   placesList.append(createCard(element, deleteCard, addLikeCard, openCard))
-});
+});*/
 
 //Открытие окна редактирования профиля
 profileEditButton.addEventListener('click', () => {
@@ -40,7 +41,7 @@ profileEditButton.addEventListener('click', () => {
 addCardButton.addEventListener('click', () => {
   const buttonElement = Array.from(document.querySelectorAll('.popup__button'));
   buttonElement.forEach((elem)=>{
-    elem.classList.add('form__submit_inactive');
+    elem.classList.add('popup__button_disabled');
     elem.disabled = true;
   })
   openModal(popupAddCard);
@@ -95,23 +96,23 @@ formNewCard.addEventListener('submit', handleFormNewCardSubmit);
 /////////////////////////////////////////////////////////////////////////////////////
 
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (formElement,inputElement,errorMessage) => {
+const showInputError = (formElement,inputElement,errorMessage,inputErrorClass,errorClass) => {
   const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
+  inputElement.classList.add(inputErrorClass);
   formError.textContent=errorMessage;
-  formError.classList.add('form__input-error_active');
+  formError.classList.add(errorClass);
 };
 
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement,inputElement) => {
+const hideInputError = (formElement,inputElement,inputErrorClass,errorClass) => {
   const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  formError.classList.remove('form__input-error_active');
+  inputElement.classList.remove(inputErrorClass);
+  formError.classList.remove(errorClass);
   formError.textContent = '';
 };
 
 // Функция, которая проверяет валидность поля
-const isValid = (formElement,inputElement) => {
+const isValid = (formElement,inputElement,inputErrorClass,errorClass) => {
 
   if (inputElement.validity.patternMismatch) {
     // встроенный метод setCustomValidity принимает на вход строку
@@ -126,10 +127,10 @@ const isValid = (formElement,inputElement) => {
 
   if (!inputElement.validity.valid) {
     // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formElement,inputElement, inputElement.validationMessage);
+    showInputError(formElement,inputElement, inputElement.validationMessage,inputErrorClass,errorClass);
   } else {
     // Если проходит, скроем
-    hideInputError(formElement,inputElement);
+    hideInputError(formElement,inputElement,inputErrorClass,errorClass);
   }
 };
 
@@ -146,50 +147,114 @@ const hasInvalidInput = (inputList) => {
 
 // Функция принимает массив полей ввода
 // и элемент кнопки, состояние которой нужно менять
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement,inactiveButtonClass) => {
   // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
     // сделай кнопку неактивной
       buttonElement.disabled = true;
-      buttonElement.classList.add('form__submit_inactive');
+      buttonElement.classList.add(inactiveButtonClass);
   } else {
         // иначе сделай кнопку активной
       buttonElement.disabled = false;
-      buttonElement.classList.remove('form__submit_inactive');
+      buttonElement.classList.remove(inactiveButtonClass);
   }
 };
 
-const setEventListeners = (formElement) => {
+const setEventListeners = (formElement,inputSelector,submitButtonSelector,inactiveButtonClass,inputErrorClass,errorClass) => {
   // Находим все поля внутри формы,
   // сделаем из них массив методом Array.from
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  const buttonElement = Array.from(document.querySelectorAll('.popup__button'));
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = Array.from(document.querySelectorAll(submitButtonSelector));
   // Обойдём все элементы полученной коллекции
   inputList.forEach((inputElement) => {
     // каждому полю добавим обработчик события input
     inputElement.addEventListener('input', () => {
       // Внутри колбэка вызовем isValid,
       // передав ей форму и проверяемый элемент
-      isValid(formElement, inputElement)
+      isValid(formElement, inputElement,inputErrorClass,errorClass)
       buttonElement.forEach((elem)=> {
-        toggleButtonState(inputList, elem);
+       toggleButtonState(inputList, elem, inactiveButtonClass);
       })
     });
   });
 };
 
-const enableValidation = () => {
+const enableValidation = (param) => {
   // Найдём все формы с указанным классом в DOM,
   // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  //const formList = Array.from(document.querySelectorAll('.popup__form'));
+  const formList = Array.from(document.querySelectorAll(param.formSelector));
 
   // Переберём полученную коллекцию
   formList.forEach((formElement) => {
     // Для каждой формы вызовем функцию setEventListeners,
     // передав ей элемент формы
-    setEventListeners(formElement);
+    setEventListeners(formElement, param.inputSelector, param.submitButtonSelector,param.inactiveButtonClass,param.inputErrorClass,param.errorClass);
   });
 };
 
+// очистка ошибок валидации вызовом clearValidation
+const clearValidation = (profileForm, param) => {
+
+}
+//Можно убрать пустой обьект
+const validationConfig ={
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
 // Вызовем функцию
-enableValidation();
+enableValidation(validationConfig);
+
+function getInfoUser() {
+ fetch('https://nomoreparties.co/v1/pwff-cohort-1/users/me', {
+    headers: {
+      authorization: '1408693c-201a-41de-afd2-34fb2c62888a'
+    }
+  })
+    .then(res => res.json())
+    .then((result) => {
+      title.textContent = result.name
+      job.textContent = result.about
+      avatar.src = result.avatar
+      console.log(result);
+    });
+}
+
+function getCardsServer() {
+  fetch('https://nomoreparties.co/v1/pwff-cohort-1/cards', {
+    headers: {
+      authorization: '1408693c-201a-41de-afd2-34fb2c62888a'
+    }
+  })
+    .then(res => res.json())
+    .then((result) => {
+      //console.log(result);
+      result.forEach(function(element) {
+        placesList.append(createCard(element, deleteCard, addLikeCard, openCard))
+      });
+    });
+}
+
+const promises = [getInfoUser(), getCardsServer()]
+
+Promise.all(promises)
+
+function updateUserData() {
+  fetch('https://nomoreparties.co/v1/pwff-cohort-1/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: '1408693c-201a-41de-afd2-34fb2c62888a',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'Marie Skłodowska Curie',
+      about: 'Physicist and Chemist'
+    })
+  });
+}
+
+updateUserData();
